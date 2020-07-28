5f68d54fd31792a37430d8e2ab9672651d03c933
DonGato1991 <juan.caspi7@gmail.com>
2020-07-28 12:54:01 -0500

    Cleanning validations of null fields, races query by year

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -1,15 +1,23 @@
 import { IResolvers } from 'graphql-tools';
-import { dataSources } from '../data/index';
 import { getWikipediaMobile } from '../lib/utils';
 
 const type: IResolvers = {
-   Season:{
-       year: parent => parent.season,
-       urlMobile: parent => getWikipediaMobile(parent.url)
-   },
-   Race:{
-       name: parent => parent.raceName
-   }
+  Season: {
+    year: (parent) => parent.season,
+    urlMobile: (parent) => getWikipediaMobile(parent.url),
+  },
+  Race: {
+    name: (parent) => parent.raceName,
+    circuit: (parent) => parent.Circuit,
+  },
+  Circuit: {
+    id: (parent) => parent.circuitId,
+    name: (parent) => parent.circuitName,
+    location: parent => parent.Location
+  },
+  Location: {
+    lng: (parent) => parent.long,
+  },
 };
 
 export default type;
